d44a6185f5762e44fa03ff9e103e76dd05f5f72a
CatsLover2006 <chancelorne@icloud.com>
2019-09-21 11:03:00 -0600

    First Commit
    Copied All Da Files

---
+++ teamcode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCommands/EndCommand.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.AutonomousCommands;
+
+public class EndCommand implements ICommand {
+    @Override
+    public boolean Run() {
+        return false;
+    }
+}
---
+++ teamcode/src/main/java/org/firstinspires/ftc/teamcode/RobotComponents/Drive.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.RobotComponents;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+// class to control the drive system
+public class Drive {
+
+    private DcMotor forwardLeftMotor = null;
+    private DcMotor forwardRightMotor = null;
+    private DcMotor rearLeftMotor = null;
+    private DcMotor rearRightMotor = null;
+
+    public Drive(HardwareMap hardwareMap){
+        forwardLeftMotor  = hardwareMap.get(DcMotor.class, "forwardLeftMotor");
+        forwardRightMotor = hardwareMap.get(DcMotor.class, "forwardRightMotor");
+        rearLeftMotor = hardwareMap.get(DcMotor.class, "rearLeftMotor");
+        rearRightMotor = hardwareMap.get(DcMotor.class, "rearRightMotor");
+
+        forwardLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        forwardRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        rearLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rearRightMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void drive(double x, double y, double r){
+
+        double forwardLeftPower     = y - x - r;
+        double forwardRightPower    = -y - x -r;
+        double rearLeftPower        = y + x - r;
+        double rearRightPower       = -y + x - r;
+
+        forwardLeftMotor.setPower(forwardLeftPower);
+        forwardRightMotor.setPower(forwardRightPower);
+        rearLeftMotor.setPower(rearLeftPower);
+        rearRightMotor.setPower(rearRightPower);
+    }
+
+
+    public void testFrontLeft(){
+        forwardLeftMotor.setPower(0.5);
+    }
+
+
+    public void testFrontRight(){
+        forwardRightMotor.setPower(0.5);
+    }
+
+    public void testRearLeft(){
+        rearLeftMotor.setPower(0.5);
+    }
+
+    public void testRearRight(){
+        rearRightMotor.setPower(0.5);
+    }
+}
