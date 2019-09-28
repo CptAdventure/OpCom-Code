9cfce3f00fc7d742235429f26315ff8cafb528ce
CatsLover2006 <chancelorne@icloud.com>
2019-09-28 11:14:11 -0600

    An actual OpMode

--- teamcode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/oldOpModes.java
+++ teamcode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/oldOpModes.java
@@ -1,13 +1,26 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.RobotComponents.Drive;
 
 @TeleOp(name="Concept Design OpMode", group = "LinearOpMode")
-public class oldOpModes {
+public class oldOpModes extends OpMode {
     private Drive drive;
-    private Gamepad gamepad;
+    private double a;
+    private float DPAD_SENSITIVITY=.5f;
 
+    @Override
+    public void init() {
+        drive = new Drive(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.dpad_right){a+=DPAD_SENSITIVITY;}
+        if(gamepad1.dpad_left){a-=DPAD_SENSITIVITY;}
+        drive.drive(gamepad1.left_stick_x,gamepad1.left_stick_y,a);
+    }
 }
